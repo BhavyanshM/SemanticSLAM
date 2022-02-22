954c01b596bbc56d15cffc29cca87ccc15a5ae0a
BhavyanshM <bhavyanshmishra@gmail.com>
2022-02-21 21:07:34 -0600

    Add method to compute segments and visualize bar charts and scatter plots with colors.

--- SceneUnderstanding/planar_region_merger.py
+++ SceneUnderstanding/planar_region_merger.py
@@ -11,6 +11,7 @@ class PlanarRegion:
         self.normal = np.array([0, 0, 0])
         self.n_patches = 0
         self.patches = []
+        self.segment_indices = [1]
 
     def print(self):
         print("{} {} {} {}".format(self.id, self.n_patches, self.center, len(self.patches)))
@@ -30,6 +31,21 @@ class PlanarRegion:
         transform = get_rotation_x(-angle)
         self.transform(transform)
 
+    def compute_ordered_segments(self):
+        patches = np.array(self.patches)
+
+        norms = np.linalg.norm(patches[:-1,:] - patches[1:,:], axis=1)
+
+        for i in range(patches.shape[0] - 2):
+            if norms[i] < 0.2:
+                self.segment_indices.append(self.segment_indices[i])
+            else:
+                self.segment_indices.append(self.segment_indices[i] + 1)
+
+        self.segment_indices = np.array(self.segment_indices)
+
+        print("Segments:", self.segment_indices.shape)
+
 
 class PlanarRegionProcessor:
     def __init__(self, path):
@@ -37,8 +53,8 @@ class PlanarRegionProcessor:
         self.files = sorted(os.listdir(self.path))
 
         self.fig = plt.figure(figsize=(18, 18))
-        plt.xlim(-3,3)
-        plt.ylim(-3,3)
+        # plt.xlim(-3,3)
+        # plt.ylim(-3,3)
 
     def load_regions_from_file(self, i):
         regions = []
@@ -55,11 +71,9 @@ class PlanarRegionProcessor:
 
             strings = f.readline().replace('\n', '').split(':')
             region.center = np.array(list(map(float, strings[1].split(','))))
-            print(region.center)
 
             strings = f.readline().replace('\n', '').split(':')
             region.normal = np.array(list(map(float, strings[1].split(','))))
-            print("Normal:", region.normal)
 
             strings = f.readline().replace('\n', '').split(':')
             region.n_patches = int(strings[1])
@@ -68,29 +82,45 @@ class PlanarRegionProcessor:
                 patch = list(map(float, f.readline().split(',')))
                 region.patches.append(np.array(patch))
 
-            region.patches = region.patches[0::3]
+            region.patches = region.patches[0::2]
             region.n_patches = len(region.patches)
 
-            region.print()
+            # region.print()
 
             regions.append(region)
         return regions
 
     def plot_region(self, regions, i, format='ro'):
         region = regions[i]
+        region.compute_ordered_segments()
         region.transform_normal_z_up()
         points = np.array([region.patches])[0]
         points = points[:, :2]
         print(points.shape)
 
-        plt.plot(points[:, 0], points[:, 1], format)
+
+        patches = np.array(region.patches)
+        indices = np.linspace(1, patches.shape[0] - 1, patches.shape[0] - 1)
+        region.segment_indices = np.append(region.segment_indices, 1)
+        colors = np.zeros(shape=(patches.shape[0], 3))
+
+        colors[:,0] = region.segment_indices * 342 % 255 / 255
+        colors[:, 1] = region.segment_indices * 123 % 255 / 255
+        colors[:, 2] = region.segment_indices * 225 % 255 / 255
+
+        # plt.bar(indices, region.segment_indices)
+
+        print("Shapes:", points[:,0].shape, colors[:,1].shape)
+
+        plt.scatter(points[:, 0], points[:, 1], c=colors)
 
     def run(self):
 
-        region_set1 = processor.load_regions_from_file(0)
-        region_set2 = processor.load_regions_from_file(5)
-        processor.plot_region(region_set1, 0, 'ro')
-        processor.plot_region(region_set2, 0, 'bo')
+        region_set1 = self.load_regions_from_file(0)
+        region_set2 = self.load_regions_from_file(5)
+
+        self.plot_region(region_set1, 0, 'ro')
+        # self.plot_region(region_set2, 0, 'bo')
         plt.show()
 
 
